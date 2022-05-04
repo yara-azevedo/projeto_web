f48fff1500199fa218b2877ee6209773e4e83552
Yara Azevedo <72475344+yara-azevedo@users.noreply.github.com>
2022-05-03 22:26:18 -0300

    A

---
+++ src/cadastro_tutor.php
@@ -0,0 +1,28 @@
+1 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <h1> Login </h1>
+        <form action = "validaLogin.php" method="post">
+        <p>
+        Usuário:
+        <input type="text" name="txtusuario">
+        </p>
+        <p>
+        Senha:
+        <input type="password" name="txtsenha">
+        </p>
+        <p>
+        <input type="submit" value="Entrar">
+        
+    </p>
+</form>
+    </div>
+</body>
+</html>

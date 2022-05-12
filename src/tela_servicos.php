e46170e2c48ec63e3eba9fc2ec3380debee75753
isabela-rodrigues <isabelarodriguescos@gmail.com>
2022-05-11 23:16:52 -0300

    tela de serviços
    Inclusão da tela de serviços - ajustes a fazer

---
+++ src/tela_servicos.php
@@ -0,0 +1,185 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style-servicos.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Tela de Serviços</title>
+    </head>
+
+    <body>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#"><img src="icons-servicos\logo.png" width="250px" height="90px"></a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a id="login" href="#">Cliente Logado</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section>
+        <div class="conteudo-servicos">
+          <div class="titulo-servicos">
+            <h1>Serviços Oferecidos</h1>
+          </div>
+          <div class="descricao-servicos">
+              <p>Para agendar clique no serviço desejado e siga as instruções.</p>
+          </div>
+        </div>
+
+        <div class="mae-bloco">
+
+          <div class="bloco">
+
+            <div class="banho">
+
+              <div class="bloco-banho">
+
+                <div  class="conteudo">
+
+                  <div class="img">
+                    <!-- <img src="https://static.thenounproject.com/png/989967-200.png" width="80px" height="80px"> -->
+                    <img src="icons-servicos/dog.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+
+                    <div class="titulo">
+                      <p>Banho e Tosa</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Pro seu pet ficar limpinho e cheirosinho</p>
+                    </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+            <div class="daycare">
+              <div class="bloco-daycare">
+
+                <div class="conteudo">
+
+                  <div class="img">
+                    <img src="icons-servicos/pet-house.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+                    <div class="titulo">
+                      <p>Day Care</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Muito mais aconchegante que uma creche comum</p>
+                    </div>
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="walker">
+              <div class="bloco-walker">
+                <div class="conteudo">
+
+                  <div class="img">
+                    <img src="icons-servicos/walking-the-dog.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+                    <div class="titulo">
+                      <p>Pet Walker</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Porque passear é uma delícia</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="petsitter">
+              <div class="bloco-petSitter">
+                <div class="conteudo">
+
+                  <div class="img">
+                    <img src="icons-servicos/dog-house.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+                    <div class="titulo">
+                      <p>Pet Sitter</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Cuidamos do seu pet com amor e carinho</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="consulta">
+              <div class="bloco-consultaVet">
+                <div class="conteudo">
+
+                  <div class="img">
+                    <img src="icons-servicos/veterinarian.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+                    <div class="titulo">
+                      <p>Consulta Veterinária</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Profissionairs prontos para tratar o seu pet</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="hospedagem">
+              <div class="bloco-hospedagem">
+                <div class="conteudo">
+
+                  <div class="img">
+                    <img src="icons-servicos/hotel.png" width="80px" height="80px">
+                  </div>
+
+                  <div class="descricao">
+                    <div class="titulo">
+                      <p>Hospedagem</p>
+                    </div>
+
+                    <div class="texto">
+                      <p>Seu pet como um membro da família</p>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      <footer>
+        <img src="icons-servicos\canto-esquerdo.png" height="300px" width="600px">
+      </footer>
+    </body>
+</html>

21afaad092ff9f9850dc39843c82f252ca260720
isabela-rodrigues <isabelarodriguescos@gmail.com>
2022-05-20 21:58:54 -0300

    inclusão da página principal
    Página construída até a parte do "quem somos"

---
+++ src/main.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Heicão</title>
+    <link rel="icon" href="../img/main/logo-pagina.png">
+</head>
+<body>
+    <section>
+      <div class="main">
+        <nav>
+          <ul>
+            <li><a href="main.php"><img src="..\img\perfil-tutor\logo.png" width="250px" height="90px"></a></li>
+            <li><a id="a" href="login.html">Login</a></li>
+            <li><a id="a" href="#">Sobre</a></li>
+            <li><a id="a" href="#">Contato</a></li>
+            <li><a id="a" href="tela_servicos.php">Serviços</a></li>
+          </ul>
+        </nav>
+        <div class="lado-direito">
+          <img id="fundo-direito" src="../img/main/contorno-direito.png">
+        </div>
+        <div class="intro">
+          <p>Suspendisse faucibus vitae justo vel consectetur.
+            Praesent ac magna ac neque ultricies viverra.
+            Suspendisse faucibus vitae justo vel consectetur.
+            Praesent ac magna ac neque ultricies viverra.
+            Suspendisse faucibus vitae justo vel consectetur.
+            Praesent ac magna ac neque ultricies viverra.</p>
+            <div class="botao-container">
+              <button id="titulo-info" class="botao"><a href="cadastro_tutor.html">Cadastre-se Já</a></button>
+            </div>
+        </div>
+        <div class="lado-esquerdo">
+            <img id="fundo-esquerdo" src="../img/main/contorno-esquerdo.png">
+        </div>
+      </div>
+      <div class="intro-empresa">
+        <div class="quem-somos">
+          <h2 id="titulo">Quem Somos</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nam accumsan sollicitudin sem, ac aliquet sem mollis at.
+            Mauris sed ullamcorper ante. Donec varius lobortis tincidunt.
+            Nam eleifend vulputate augue eget consectetur.
+            Sed posuere pretium sodales. Suspendisse lobortis laoreet velit in porta.
+            Aliquam ut semper urna, id dignissim lectus.
+            Fusce efficitur tortor et venenatis vestibulum.
+            Aliquam sollicitudin a odio vitae varius. Cras ac turpis arcu.
+            In id posuere nulla. Pellentesque at posuere nunc. In fringilla tortor id odio vestibulum varius.</p>
+        </div>
+      </div>
+      <div class="review">
+        <h2 id="titulo">O Que Dizem Nossos CLientes</h2>
+        <div class="blocos">
+          <div class="bloco-1">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nam accumsan sollicitudin sem, ac aliquet sem mollis at.
+              Mauris sed ullamcorper ante. Donec varius lobortis tincidunt.
+              Nam eleifend vulputate augue eget consectetur.
+              Sed posuere pretium sodales. Suspendisse lobortis laoreet velit in porta.
+              Aliquam ut semper urna, id dignissim lectus.
+              Fusce efficitur tortor et venenatis vestibulum.
+              Aliquam sollicitudin a odio vitae varius. Cras ac turpis arcu.
+              In id posuere nulla. Pellentesque at posuere nunc. In fringilla tortor id odio vestibulum varius.</p>
+          </div>
+          <div class="bloco-2">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nam accumsan sollicitudin sem, ac aliquet sem mollis at.
+              Mauris sed ullamcorper ante. Donec varius lobortis tincidunt.
+              Nam eleifend vulputate augue eget consectetur.
+              Sed posuere pretium sodales. Suspendisse lobortis laoreet velit in porta.
+              Aliquam ut semper urna, id dignissim lectus.
+              Fusce efficitur tortor et venenatis vestibulum.
+              Aliquam sollicitudin a odio vitae varius. Cras ac turpis arcu.
+              In id posuere nulla. Pellentesque at posuere nunc. In fringilla tortor id odio vestibulum varius.</p>
+          </div>
+          <div class="bloco-3">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nam accumsan sollicitudin sem, ac aliquet sem mollis at.
+              Mauris sed ullamcorper ante. Donec varius lobortis tincidunt.
+              Nam eleifend vulputate augue eget consectetur.
+              Sed posuere pretium sodales. Suspendisse lobortis laoreet velit in porta.
+              Aliquam ut semper urna, id dignissim lectus.
+              Fusce efficitur tortor et venenatis vestibulum.
+              Aliquam sollicitudin a odio vitae varius. Cras ac turpis arcu.
+              In id posuere nulla. Pellentesque at posuere nunc. In fringilla tortor id odio vestibulum varius.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
